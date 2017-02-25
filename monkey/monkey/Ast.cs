2c65e4a8fd0eba731b9cd753b10346af0b26ca3b
Geert Van Laethem <geert.vanlaethem@gmail.com>
2017-02-25 09:26:52 +0100

    Working on the AST and the tokenizer

---
+++ monkey/monkey/Ast.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monkey
+{
+  interface INode
+  {
+    string TokenLiteral();
+
+    string ComposeString();
+  }
+
+  interface IStatement : INode
+  {
+    void StatementNode();
+  }
+
+  interface IExpression : INode
+  {
+    void ExpressionNode();
+  }
+
+  class AstProgram : INode
+  {
+    private IList<IStatement> Statements { get; set; }
+
+    public string TokenLiteral()
+    {
+      return Statements.Count > 0 ? Statements[0].TokenLiteral() : string.Empty;
+    }
+
+    public string ComposeString()
+    {
+      var buffer = new StringBuilder();
+
+      foreach (var statement in Statements)
+      {
+        buffer.Append(statement.ComposeString());
+      }
+
+      return buffer.ToString();
+    }
+  }
+
+  class Identifier : INode
+  {
+    public Token Token { get; set; }
+    public IExpression Value { get; set; }
+    public string TokenLiteral()
+    {
+      return Token.Literal;
+    }
+
+    public string ComposeString()
+    {
+      return Value.ComposeString();
+    }
+  }
+
+  class LetStatement : IStatement
+  {
+    public Token Token { get; set; }
+    public Identifier Name { get; set; }
+    public IExpression Value { get; set; }
+    public string TokenLiteral()
+    {
+      return Token.Literal;
+    }
+
+    public string ComposeString()
+    {
+      var buffer = new StringBuilder();
+
+      buffer.Append(Token.Literal + " ");
+      buffer.Append(Name.ComposeString());
+      buffer.Append(" = ");
+
+      if (Value != null)
+      {
+        buffer.Append(Value.ComposeString());
+      }
+
+      buffer.Append(";");
+
+      return buffer.ToString();
+    }
+
+    public void StatementNode()
+    {
+    }
+  }
+
+  class ReturnStatement : IStatement
+  {
+    public Token Token { get; set; }
+
+    public string TokenLiteral()
+    {
+      return Token.Literal;
+    }
+
+    public string ComposeString()
+    {
+      var buffer = new StringBuilder();
+
+
+
+      return buffer;
+    }
+
+    public void StatementNode()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ monkey/monkey/Token.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace monkey
+{
+  public class TokenType
+  {
+    public const string Illegal = "ILLEGAL";
+    public const string Eof = "EOF";
+    public const string Ident = "IDENT";
+    public const string Int = "INT";
+    public const string Assign = "=";
+    public const string Plus = "+";
+    public const string Minus = "-";
+    public const string Bang = "!";
+    public const string Asterisk = "*";
+    public const string Slash = "/";
+    public const string Comma = ",";
+    public const string SemiColon = ";";
+    public const string LParen = "(";
+    public const string RParen = ")";
+    public const string LBrace = "[";
+    public const string RBrace = "]";
+    public const string Lt = "<";
+    public const string Rt = ">";
+    public const string Function = "FUNCTION";
+    public const string Let = "LET";
+    public const string True = "TRUE";
+    public const string False = "FALSE";
+    public const string If = "IF";
+    public const string Else = "ELSE";
+    public const string Return = "RETURN";
+    public const string Eq = "==";
+    public const string NotEq = "!=";
+  }
+
+  public class KeyWords : Dictionary<string, string>
+  {
+    public KeyWords()
+    {
+      Add("fn", TokenType.Function);
+      Add("let", TokenType.Let);
+      Add("true", TokenType.True);
+      Add("false", TokenType.False);
+      Add("if", TokenType.If);
+      Add("else", TokenType.Else);
+      Add("return", TokenType.Return);
+    }
+
+    public string LookupIdent(string identifier)
+    {
+      if (ContainsKey(identifier))
+      {
+        return this[identifier];
+      }
+
+      return TokenType.Ident;
+    }
+  }
+
+  class Token
+  {
+    public TokenType Type { get; set; }
+    public string Literal { get; set; }
+  }
+}
